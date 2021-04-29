b2c6b7269f1db88d0e2399c223aab192045f00a9
Ryan Bartling <ryan.bartling@thinkdeca.com>
2021-04-29 10:30:24 -0700

    docs: Move install-requires comment

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import setuptools
 
 # setup.py is required in order to install in editable mode
+
 # install_requires is here instead of setup.cfg for github dependency graph
+# This fills a different role from requirements.txt
+# https://packaging.python.org/discussions/install-requires-vs-requirements/
 
 setuptools.setup(name="hello", install_requires=["click"])
